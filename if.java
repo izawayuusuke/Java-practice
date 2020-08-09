211ad309ca9a8dc99395c6e9baa962a0e2428601
izawa yuusuke <yuu05020@gmail.com>
2020-08-09 16:19:32 +0900

    if文で時刻判定

---
+++ if.java
@@ -0,0 +1,20 @@
+public class IF {
+  public static void main(String[] args) {
+    int time = 4;
+    // System.out.print("只今の時刻は" + time + "時です。");
+    // System.out.print("おはようございます。");
+    if ((time >= 4) && (time <= 10)) {
+      System.out.print("只今の時刻は" + time + "時です。");
+      System.out.print("おはようございます。");
+    } else if ((time >= 11) && (time <= 17)) {
+      System.out.print("只今の時刻は" + time + "時です。");
+      System.out.print("こんにちは");
+    } else if ((time == 18)) {
+      System.out.print("只今の時刻は" + time + "時です。");
+      System.out.print("こんばんは");
+    } else if ((time >= 19) || (time <= 3)) {
+      System.out.print("只今の時刻は" + time + "時です。");
+      System.out.print("おやすみなさい");
+    }
+  }
+}
