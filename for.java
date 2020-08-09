b0d13872726008a86a12d7cb8b69573c0a0b3405
izawa yuusuke <yuu05020@gmail.com>
2020-08-09 17:00:09 +0900

    for文で累乗計算

---
+++ for.java
@@ -0,0 +1,12 @@
+public class For {
+  public static void main(String[] args) {
+    int answer = 1;
+
+    for (int i = 0; i < 5; i++) {
+      answer = answer * 2;
+    }
+
+    System.out.print("2の5乗は、");
+    System.out.print(answer);
+  }
+}
