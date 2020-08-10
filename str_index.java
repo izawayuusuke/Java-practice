cb664b050bfbf54e6cdd4b6855d5c2699744445d
izawa yuusuke <yuu05020@gmail.com>
2020-08-10 17:22:13 +0900

    文字列をcharAtメソッドで変換後char型の変数に代入する

--- str_index.java
+++ str_index.java
@@ -3,5 +3,11 @@ public class StringIndex {
     String str = "Hello";
     System.out.println(str.indexOf("Hello"));
     System.out.println(str.substring(0, 3));
+
+    char ch = str.charAt(0);
+
+    System.out.println(str);
+    System.out.println(str.charAt(0));
+    System.out.println(ch);
   }
 }
