c94cc19c2900fd18f5c7ab2fc00e28f9992e1fd9
izawa yuusuke <yuu05020@gmail.com>
2020-08-10 17:08:23 +0900

    文字列メソッド

---
+++ str_index.java
@@ -0,0 +1,7 @@
+public class StringIndex {
+  public static void main(String[] args) {
+    String str = "Hello";
+    System.out.println(str.indexOf("Hello"));
+    System.out.println(str.substring(0, 3));
+  }
+}
